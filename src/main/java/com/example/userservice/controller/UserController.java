e4ded2be95fa5e1c5e8bc225d99c48f39d620dac
Madelene Axblom <90625377+GreenGard@users.noreply.github.com>
2022-05-11 19:01:38 +0200

    Update UserController.java

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -2,11 +2,9 @@ package com.example.userservice.controller;
 
 import com.example.userservice.entity.User;
 import com.example.userservice.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.list;
 
 @RestController
 @RequestMapping("users")
