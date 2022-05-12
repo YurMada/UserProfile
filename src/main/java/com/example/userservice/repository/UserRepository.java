91f0dd136d9c7b0545aa85768c1803cc18bb326c
Madelene <you@example.com>
2022-05-12 08:21:31 +0200

    fix imports

--- src/main/java/com/example/userservice/repository/UserRepository.java
+++ src/main/java/com/example/userservice/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.example.userservice.repository;
 import com.example.userservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     User findUserById(String id);
