76a353acb88f6ab88b8cd6e8d5f3d3509b15fa94
Madelene <you@example.com>
2022-05-19 16:46:57 +0200

    get Couldn't find bearer string, header will be ignored

--- src/main/java/com/example/userservice/repository/UserRepository.java
+++ src/main/java/com/example/userservice/repository/UserRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends CrudRepository<User, Long> {
 
 
-    User findByUsername(String username);
+   // User findByUsername(String userName);
 }
